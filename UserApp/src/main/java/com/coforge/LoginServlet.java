5dde32228076e7c3a3bfc8cafa2004ea442273a0
swapna2021 <swapna.motupally@gmail.com>
2026-03-05 22:58:04 +0530

    commiting UserApp

---
+++ UserApp/src/main/java/com/coforge/LoginServlet.java
@@ -0,0 +1,63 @@
+package com.coforge;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public LoginServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		PrintWriter out=response.getWriter();
+		String uname=request.getParameter("uname");
+		String pwd=request.getParameter("pwd");
+		RequestDispatcher rd=null;
+		
+		if(uname.equals("swapna")&&pwd.equals("swapna@123")) {
+			request.setAttribute("username", uname);
+			rd=request.getRequestDispatcher("/welcome.jsp");
+			rd.forward(request, response);
+		}
+				 
+		else
+		{
+			rd=request.getRequestDispatcher("/error.jsp");
+			rd.forward(request, response);
+		}
+		out.close();
+		
+		
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ UserApp/src/main/webapp/index.jsp
@@ -0,0 +1,12 @@
+<html>
+<body>
+<h2><%= "Hello World!" %></h2>
+<form action="login.jsp">
+	<button>SignIn</button>
+</form>
+
+<form action="signup.jsp">
+	<button>SignUp</button>
+</form>
+</body>
+</html>
